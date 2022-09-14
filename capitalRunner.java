de20796024d66f2a35ef97c318a6cf99c55a2678
jeevika jeevika <jeevikags.xworkz@gmail.com>
2022-09-14 19:55:41 +0530

    program

---
+++ capitalRunner.java
@@ -0,0 +1,8 @@
+class capitalRunner
+{
+	public static void main(String[] args)
+	{
+		String value=capital.getCapitalByState("Assam");
+		System.out.println(value);
+	}
+}	
---
+++ nameRunner.java
@@ -0,0 +1,7 @@
+class nameRunner{
+	public static void main(String[] args)
+   {
+      String no=names.getNameByMobile(981173637);
+      System.out.println(no);
+   }
+}
