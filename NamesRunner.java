86af0948c32029bbdcadc676cdae9b799a446136
jeevika jeevika <jeevikags.xworkz@gmail.com>
2022-09-18 18:53:14 +0530

    get name and capital programs

---
+++ NamesRunner.java
@@ -0,0 +1,25 @@
+class NamesRunner{
+	public static void main(String[] args)
+   {
+      String ref=Names.getNameByMobile(981173637);
+      System.out.println(ref);
+	  String ref1=Names.getNameByMobile(971222647);
+      System.out.println(ref1);
+	  String ref2=Names.getNameByMobile(927161727);
+      System.out.println(ref2);
+	  String ref3=Names.getNameByMobile(937141727);
+      System.out.println(ref3);
+	  String ref4=Names.getNameByMobile(947361727);
+      System.out.println(ref4);
+	  String ref5=Names.getNameByMobile(947161727);
+      System.out.println(ref5);
+	  String ref6=Names.getNameByMobile(957161724);
+      System.out.println(ref6);
+	  String ref7=Names.getNameByMobile(967161728);
+      System.out.println(ref7);
+	  String ref8=Names.getNameByMobile(957161727);
+      System.out.println(ref8);
+	  String ref9=Names.getNameByMobile(987161727);
+      System.out.println(ref9);
+   }
+}
---
+++ capitalRunner.java
@@ -0,0 +1,65 @@
+class capitalRunner
+{
+	public static void main(String[] args)
+	{
+		String value1=capital.getCapitalByState("Maharashtra");
+		System.out.println(value1);
+		String value2=capital.getCapitalByState("AndhraPradesh");
+		System.out.println(value2);
+		String value3=capital.getCapitalByState("ArunachalPradesh");
+		System.out.println(value3);
+		String value4=capital.getCapitalByState("Assam");
+		System.out.println(value4);
+		String value5=capital.getCapitalByState("Bihar");
+		System.out.println(value5);
+		String value6=capital.getCapitalByState("Chattisgarh");
+		System.out.println(value6);
+		String value7=capital.getCapitalByState("Goa");
+		System.out.println(value7);
+		String value8=capital.getCapitalByState("Gujarat");
+		System.out.println(value8);
+		String value9=capital.getCapitalByState("Harayana");
+		System.out.println(value9);
+		String value10=capital.getCapitalByState("HimachalPradesh");
+		System.out.println(value10);
+		String value11=capital.getCapitalByState("Jharkhand");
+		System.out.println(value11);
+		String value12=capital.getCapitalByState("Karnataka");
+		System.out.println(value12);
+		String value13=capital.getCapitalByState("Kerala");
+		System.out.println(value13);
+		String value14=capital.getCapitalByState("MadhyaPradesh");
+		System.out.println(value14);
+		String value15=capital.getCapitalByState("Manipur");
+		System.out.println(value15);
+		String value16=capital.getCapitalByState("Meghalaya");
+		System.out.println(value16);
+		String value17=capital.getCapitalByState("Mizoram");
+		System.out.println(value17);
+		String value18=capital.getCapitalByState("Nagaland");
+		System.out.println(value18);
+		String value19=capital.getCapitalByState("Odisha");
+		System.out.println(value19);
+		String value20=capital.getCapitalByState("Punjab");
+		System.out.println(value20);
+		String value21=capital.getCapitalByState("Rajasthan");
+		System.out.println(value21);
+		String value22=capital.getCapitalByState("Sikkim");
+		System.out.println(value22);
+		String value23=capital.getCapitalByState("TamilNadu");
+		System.out.println(value23);
+		String value24=capital.getCapitalByState("Telangana");
+		System.out.println(value24);
+		String value25=capital.getCapitalByState("Tripura");
+		System.out.println(value25);
+		String value26=capital.getCapitalByState("UttarPradesh");
+		System.out.println(value26);
+		String value27=capital.getCapitalByState("Uttarkhand");
+		System.out.println(value27);
+		String value28=capital.getCapitalByState("WestBengal");
+		System.out.println(value28);
+		String value29=capital.getCapitalByState("JammuAndKashmir");
+		System.out.println(value29);
+		
+	}
+}	
