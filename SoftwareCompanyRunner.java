2a42534d6dc01e3a62284f734ebb52bc5f171e95
jeevika jeevika <jeevikags.xworkz@gmail.com>
2022-09-22 20:35:34 +0530

    programs

---
+++ SoftwareCompanyRunner.java
@@ -0,0 +1,38 @@
+class  SoftwareCompanyRunner{
+	public static void main(String[] value){
+		System.out.println("Started executing main method:");
+		
+		String ref= value[0];
+		System.out.println("index 0:"+ref);
+		
+		String ref1= value[1];
+		System.out.println("index 1:"+ref1);
+		
+		String ref2= value[2];
+		System.out.println("index 2:"+ref2);
+		
+		String ref3= value[3];
+		System.out.println("index 3:"+ref3);
+		
+		String ref4= value[4];
+		System.out.println("index 4:"+ref4);
+		
+		String ref5= value[5];
+		System.out.println("index 5:"+ref5);
+		
+		String ref6= value[6];
+		System.out.println("index 6:"+ref6);
+		
+		String ref7= value[7];
+		System.out.println("index 7:"+ref7);
+		
+		String ref8= value[8];
+		System.out.println("index 8:"+ref8);
+		
+		String ref9= value[9];
+		System.out.println("index 9:"+ref9);
+		
+		System.out.println("Total Values:"+value.length);
+		System.out.println("end main method:");
+	}
+}	
